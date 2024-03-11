5fb29167b8851dac2093b4ed72f3a842e54c85f1
Cassio Cintra <cassiocr2012@hotmail.com>
2024-03-11 14:08:20 -0300

    Design Patters Singleton

---
+++ src/Singleton/SingletonLazyHolder.java
@@ -0,0 +1,19 @@
+package Singleton;
+
+/**
+ *  Singleton "Lazy Holder".
+ *  @see <a href="https://stackoverflow.com/a/24018148"> Reference </a>
+ */
+public class SingletonLazyHolder {
+    private static class InstanceHolder {
+        public static SingletonLazyHolder instance = new SingletonLazyHolder();
+    }
+
+    private SingletonLazyHolder() {
+        super();
+    }
+
+    public static SingletonLazyHolder getInstance() {
+        return InstanceHolder.instance;
+    }
+}
